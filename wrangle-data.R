fabc412a28be2228b91955ba840efc14a71b2c9e
Ronald Chow <68787037+jh-ronald@users.noreply.github.com>
2020-11-27 00:20:48 +0800

    Adjustments to the code so that it returns a data.table instead of a spec_tbl_df.

--- wrangle-data.R
+++ wrangle-data.R
@@ -1,4 +1,6 @@
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
 library(tidyverse)
+library(data.table)
 
-heartfailure <- read_csv(dl)
+heartfailure <- data.table(read_csv(dl))
